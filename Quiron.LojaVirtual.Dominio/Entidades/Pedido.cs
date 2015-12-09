f886d0fefcceec8682d866c364ada5aae3723a9c
plakteus <plakteus@hotmail.com>
2015-12-09 18:58:11 -0200

    Implementação do carrinho de compras

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage = "Informe seu nome")]
+        public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Informe o CEP")]
+        [Display(Name = "Cep:")]
+        public string CEP { get; set; }
+
+        [Display(Name = "Endereço:")]
+        [Required(ErrorMessage = "Informe o endereço")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Display(Name = "Cidade:")]
+        [Required(ErrorMessage = "Informe a Cidade")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Bairro:")]
+        [Required(ErrorMessage = "Informe o Bairro")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "Email:")]
+        [Required(ErrorMessage = "Informe o Email")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        public bool EmbrulhaPresente { get; set; }
+
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,77 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutoRepositorio _repositorio;
+
+        public ViewResult Index(string returnURL)
+        {
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                ReturnURL = returnURL
+            });
+        }
+
+
+        public RedirectToRouteResult Adicionar(int IdProduto, string returnURL)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.IdProduto == IdProduto);
+
+            if (produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnURL });
+        }
+
+        public RedirectToRouteResult Remover(int IdProduto, string returnURL)
+        {
+            _repositorio = new ProdutoRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.IdProduto == IdProduto);
+
+            if (produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+
+            return RedirectToAction("Index", new { returnURL });
+        }
+
+
+        public PartialViewResult Resumo()
+        {
+            Carrinho carrinho = ObterCarrinho();
+
+            return PartialView(carrinho);
+        }
+
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho)Session["Carrinho"];
+
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+    }
+}
